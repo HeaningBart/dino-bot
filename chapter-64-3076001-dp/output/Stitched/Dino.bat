9933dc2fa18759ad0089ee95df6228a86b5c2db7
Gustavo Souza Araujo <gustavo.araujo@aluno.uepa.br>
2022-12-15 01:04:33 -0300

    error

--- chapter-64-3076001-dp/output/Stitched/Dino.bat
+++
@@ -1,5 +0,0 @@
-python3.9 C:\SmartStitchConsole.py -i "JPEG" -H 12000 -cw 800 -w 2 -t ".jpeg" -s 90
-mkdir CUNet
-C:\waifu2x-ncnn-vulkan.exe -n 3 -s 1 -o ./CUNet -i ./Stitched -f jpg -j 2:2:2
-C:/Rar.exe a CUNet.rar CUNet/*
-rd /s /q "Stitched"
