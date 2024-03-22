d4339227af0c4b09e490860c8b44439e5b134433
Gustavo Souzavs <gustavo.araujo@aluno.uepa.br>
2024-03-22 18:36:24 -0300

    added a few stuff

--- src/queue/raws.ts
+++ src/queue/raws.ts
@@ -1,28 +1,28 @@
-import Queue from "bull";
-import { getLatestChapter, getSpecificChapter } from "../rawhandler";
+import Queue from 'bull'
+import { getLatestChapter, getSpecificChapter } from '../rawhandler'
 import {
   getLezhinSpecificChapter,
   getSeriesInfo,
   startup,
-} from "../rawhandler/lezhin";
-import { client } from "../client";
-import { randomUUID } from "node:crypto";
-import { getRidiChapter } from "../rawhandler/ridibooks";
+} from '../rawhandler/lezhin'
+import { client } from '../client'
+import { randomUUID } from 'node:crypto'
+import { getRidiChapter, logIn } from '../rawhandler/ridibooks'
 
 export const rawsQueue = new Queue<RawsPayload>(
   randomUUID(),
-  "redis://127.0.0.1:6379"
-);
+  'redis://127.0.0.1:6379'
+)
 
 export type RawsPayload = {
-  kakaoId: string;
-  chapter_number?: string;
-  series_title?: string;
-  channel_id: string;
-  role_id?: string;
-  command: "getchapter" | "weekly";
-  type: "kakao" | "lezhin" | "pyccoma" | "ridi";
-};
+  kakaoId: string
+  chapter_number?: string
+  series_title?: string
+  channel_id: string
+  role_id?: string
+  command: 'getchapter' | 'weekly'
+  type: 'kakao' | 'lezhin' | 'pyccoma' | 'ridi'
+}
 
 rawsQueue.process(async (job, done) => {
   const {
@@ -33,62 +33,63 @@ rawsQueue.process(async (job, done) => {
     command,
     chapter_number,
     type,
-  } = job.data;
+  } = job.data
 
-  console.log(job.data);
+  console.log(job.data)
 
-  if (type === "kakao") {
+  if (type === 'kakao') {
     const chapter =
-      command === "weekly"
+      command === 'weekly'
         ? await getLatestChapter(kakaoId, series_title!)
-        : await getSpecificChapter(kakaoId, chapter_number!, kakaoId);
+        : await getSpecificChapter(kakaoId, chapter_number!, kakaoId)
     if (chapter) {
-      const channel = client.channels.cache.get(channel_id);
+      const channel = client.channels.cache.get(channel_id)
       if (channel?.isText()) {
-        command === "weekly"
+        command === 'weekly'
           ? await channel.send(
               `Weekly chapter of ${series_title}, <@&${role_id}>, <@&946250134042329158>: ${chapter}`
             )
-          : await channel.send(`Chapter: ${chapter}`);
+          : await channel.send(`Chapter: ${chapter}`)
 
         await channel.send(
           `Don't forget to report your progress in <#794058643624034334> after you are done with your part.`
-        );
+        )
       }
     }
   }
 
-  if (type === "lezhin") {
-    const chapter = await getLezhinSpecificChapter(kakaoId, chapter_number!);
-    const channel = client.channels.cache.get(channel_id);
+  if (type === 'lezhin') {
+    const chapter = await getLezhinSpecificChapter(kakaoId, chapter_number!)
+    const channel = client.channels.cache.get(channel_id)
     if (channel?.isText()) {
-      command === "weekly"
+      command === 'weekly'
         ? await channel.send(
             `Chapter of ${series_title}, <@&${role_id}>, <@&946250134042329158>: ${chapter}`
           )
-        : await channel.send(`Chapter: ${chapter}`);
+        : await channel.send(`Chapter: ${chapter}`)
 
       await channel.send(
         `Don't forget to report your progress in <#794058643624034334> after you are done with your part.`
-      );
+      )
     }
   }
 
-  if (type === "ridi") {
-    const chapter = await getRidiChapter(kakaoId, chapter_number!);
-    const channel = client.channels.cache.get(channel_id);
+  if (type === 'ridi') {
+    await logIn()
+    const chapter = await getRidiChapter(kakaoId, chapter_number!)
+    const channel = client.channels.cache.get(channel_id)
     if (channel?.isText()) {
-      command === "weekly"
+      command === 'weekly'
         ? await channel.send(
             `Chapter of ${series_title}, <@&${role_id}>, <@&946250134042329158>: ${chapter}`
           )
-        : await channel.send(`Chapter: ${chapter}`);
+        : await channel.send(`Chapter: ${chapter}`)
 
       await channel.send(
         `Don't forget to report your progress in <#794058643624034334> after you are done with your part.`
-      );
+      )
     }
   }
 
-  done();
-});
+  done()
+})
