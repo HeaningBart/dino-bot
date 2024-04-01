21ec899e31a7c5164687cbada9096dee635c275a
Gustavo Souzavs <gustavo.araujo@aluno.uepa.br>
2024-04-01 00:07:51 -0300

    test

--- src/queue/worker.ts
+++ src/queue/worker.ts
@@ -1,6 +1,91 @@
-import { Worker } from 'bullmq'
+import { Worker, Job } from 'bullmq'
 import path from 'path'
+import { RawsPayload } from './raws.js'
+import { getLatestChapter, getSpecificChapter } from '../rawhandler/index.js'
+import { getLezhinSpecificChapter } from '../rawhandler/lezhin/index.js'
+import { client } from '../client/index.js'
+import { getRidiChapter, logIn } from '../rawhandler/ridibooks/index.js'
+import { getBoomToonChapter } from '../rawhandler/boomtoon/index.js'
 
 const processorFile = path.join(__dirname, 'processor.js')
 console.log(processorFile)
-export const worker = new Worker('bot_raws', processorFile)
+
+export const worker = new Worker('bot_raws', async function (
+  job: Job<RawsPayload>
+) {
+  const {
+    channel_id,
+    kakaoId,
+    role_id,
+    series_title,
+    command,
+    chapter_number,
+    type,
+  } = job.data
+  console.log(job.data)
+  if (type === 'kakao') {
+    const chapter =
+      command === 'weekly'
+        ? await getLatestChapter(kakaoId, series_title!)
+        : await getSpecificChapter(kakaoId, chapter_number!, kakaoId)
+    if (chapter) {
+      const channel = client.channels.cache.get(channel_id)
+      if (channel?.isText()) {
+        command === 'weekly'
+          ? await channel.send(
+              `Weekly chapter of ${series_title}, <@&${role_id}>, <@&946250134042329158>: ${chapter}`
+            )
+          : await channel.send(`Chapter: ${chapter}`)
+
+        await channel.send(
+          `Don't forget to report your progress in <#794058643624034334> after you are done with your part.`
+        )
+      }
+    }
+  }
+  if (type === 'lezhin') {
+    const chapter = await getLezhinSpecificChapter(kakaoId, chapter_number!)
+    const channel = client.channels.cache.get(channel_id)
+    if (channel?.isText()) {
+      command === 'weekly'
+        ? await channel.send(
+            `Chapter of ${series_title}, <@&${role_id}>, <@&946250134042329158>: ${chapter}`
+          )
+        : await channel.send(`Chapter: ${chapter}`)
+
+      await channel.send(
+        `Don't forget to report your progress in <#794058643624034334> after you are done with your part.`
+      )
+    }
+  }
+  if (type === 'ridi') {
+    const chapter = await getRidiChapter(kakaoId, chapter_number!)
+    const channel = client.channels.cache.get(channel_id)
+    if (channel?.isText()) {
+      command === 'weekly'
+        ? await channel.send(
+            `Chapter of ${series_title}, <@&${role_id}>, <@&946250134042329158>: ${chapter}`
+          )
+        : await channel.send(`Chapter: ${chapter}`)
+
+      await channel.send(
+        `Don't forget to report your progress in <#794058643624034334> after you are done with your part.`
+      )
+    }
+  }
+  if (type === 'boomtoon') {
+    const chapter = await getBoomToonChapter(kakaoId, chapter_number!)
+    const channel = client.channels.cache.get(channel_id)
+    if (channel?.isText()) {
+      command === 'weekly'
+        ? await channel.send(
+            `Chapter of ${series_title}, <@&${role_id}>, <@&946250134042329158>: ${chapter}`
+          )
+        : await channel.send(`Chapter: ${chapter}`)
+
+      await channel.send(
+        `Don't forget to report your progress in <#794058643624034334> after you are done with your part.`
+      )
+    }
+  }
+})
