972df927c485d675eed412b52c1f5fbc549d6a13
Gustavo Souzavs <gustavo.araujo@aluno.uepa.br>
2024-04-01 00:05:27 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 import { client } from './client/index.js'
-import { rawsQueue } from './queue/raws.js'
-;(async function () {
-  console.log(await rawsQueue.getWorkers())
-})()
 
 export { client }
