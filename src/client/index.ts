ebac1304b3dcf26384dbfa07a993481bfdad3a20
Gustavo Souzavs <gustavo.araujo@aluno.uepa.br>
2024-04-01 11:24:21 -0300

    fixed

--- src/client/index.ts
+++ src/client/index.ts
@@ -69,10 +69,11 @@ client.on('ready', async () => {
 
 console.log(token)
 
-client.on(Events.InteractionCreate, async (interaction) => {
+client.once(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isCommand()) return
   try {
     await interaction.deferReply()
+    interaction
     const command = client.commands.get(interaction.commandName)
     if (!command) {
       console.error(`No command matching ${interaction.commandName} was found.`)
