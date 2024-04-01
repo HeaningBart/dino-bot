d24ea1d3db0ba1b3c70a52322d6c16c95c83630b
Gustavo Souzavs <gustavo.araujo@aluno.uepa.br>
2024-04-01 00:13:59 -0300

    test

--- src/queue/raws.ts
+++ src/queue/raws.ts
@@ -1,16 +1,7 @@
-import { Queue, Worker } from 'bullmq'
-import path from 'path'
-import { fileURLToPath, pathToFileURL } from 'url'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import { Queue } from 'bullmq'
 
 export const rawsQueue = new Queue<RawsPayload>('bot_raws')
-const processorFile = pathToFileURL(__dirname + '/processor.js')
-export const worker = new Worker('bot_raws', processorFile, {
-  connection: {},
-  autorun: true,
-  concurrency: 1,
-})
+
 export type RawsPayload = {
   kakaoId: string
   chapter_number?: string
