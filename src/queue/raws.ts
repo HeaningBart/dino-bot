d5491c938dcc7e5fe6d7d25525dfc00dfe375149
Gustavo Souzavs <gustavo.araujo@aluno.uepa.br>
2024-04-02 10:05:08 -0300

    fixed

--- src/queue/raws.ts
+++ src/queue/raws.ts
@@ -2,12 +2,23 @@ import { Queue } from 'bullmq'
 import { Worker } from 'bullmq'
 import path from 'path'
 import { fileURLToPath, pathToFileURL } from 'url'
+import { redis } from '../redis/index.js'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-export const rawsQueue = new Queue<RawsPayload>('bot_raws')
+export const rawsQueue = new Queue<RawsPayload>('bot_raws', {
+  connection: {
+    host: '127.0.0.1',
+    port: 6379,
+    db: 1,
+  },
+})
 const processorFile = pathToFileURL(__dirname + '/processor.js')
 export const worker = new Worker('bot_raws', processorFile, {
-  connection: {},
+  connection: {
+    host: '127.0.0.1',
+    port: 6379,
+    db: 1,
+  },
   autorun: true,
   concurrency: 1,
 })
