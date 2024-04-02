77e0720b6650fbb7513f6200e06921a19f289537
Gustavo Souzavs <gustavo.araujo@aluno.uepa.br>
2024-04-02 09:47:22 -0300

    fixed

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,4 @@ import { Job } from 'bullmq'
 import { client } from './client/index.js'
 import { rawsQueue, worker, RawsPayload } from './queue/raws.js'
 
-client.once('ready', async () => {
-  client.user &&
-    client.user.setActivity(`Chapters on the queue: ${await rawsQueue.count()}`)
-})
-
-worker.on('drained', async () => {
-  client.user &&
-    client.user.setActivity(`Chapters on the queue: ${await rawsQueue.count()}`)
-})
-
-worker.on('completed', async (job: Job<RawsPayload>) => {
-  client.user &&
-    client.user.setActivity(
-      `I finished chapter ${job.data.chapter_number} of ${job.data.kakaoId}!`
-    )
-})
-
-worker.on('progress', async (job: Job<RawsPayload>) => {
-  client.user &&
-    client.user.setActivity(
-      `I'm working on chapter ${job.data.chapter_number} of ${job.data.kakaoId}!`
-    )
-})
-
 export { client }
